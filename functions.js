dfffcfe6b739101156df0118b77211fb3e880e3c
Liam Dawson <liamrdawson@gmail.com>
2019-10-23 20:30:10 +0100

    add falsy functions and tests

--- functions.js
+++ functions.js
@@ -1,5 +1,7 @@
 const functions = {
-    add: (num1, num2) => num1 + num2
+    add: (num1, num2) => num1 + num2,
+    isNull: () => null,
+    checkFalsy: x => x
 }
 
 module.exports = functions;
--- functions.test.js
+++ functions.test.js
@@ -3,3 +3,28 @@ const functions = require('./functions');
 test('Adds 2 + 2 to equal 4', () => {
     expect(functions.add(2, 2)).toBe(4);
 });
+
+test('Adds 2 + 2 to NOT equal 5', () => {
+    expect(functions.add(2, 2)).not.toBe(5);
+});
+
+
+/**
+ *  NOTES
+ *  
+ *  TO CHECK FOR TRUTHY OR FALSY VALUES...
+ *  .toBeNull       matches  null
+ *  .toBeUndefined  matches  undefined
+ *  .toBeDefined    matches  undefined
+ *  .toBeTruthy     matches  truthy
+ *  .toBeFalsy      matches  falsy
+ * 
+ */
+
+test('Should be null', () => {
+    expect(functions.isNull()).toBeNull();
+});
+
+test('Should be falsy', () => {
+    expect(functions.checkValue(null)).toBeFalsy();
+});
