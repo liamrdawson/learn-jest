3a778b38c868dedf3fde0316506312180522c934
Liam Dawson <liamrdawson@gmail.com>
2019-10-23 21:03:36 +0100

    set up tests using basic matchers

--- functions.js
+++ functions.js
@@ -1,7 +1,15 @@
 const functions = {
     add: (num1, num2) => num1 + num2,
     isNull: () => null,
-    checkFalsy: x => x
+    checkFalsy: x => x,
+    createUser: () => {
+        const user = {
+            firstName: "Liam"
+        };
+        user["lastName"] = "Dawson";
+        return user;
+    }
+
 }
 
 module.exports = functions;
--- functions.test.js
+++ functions.test.js
@@ -1,9 +1,11 @@
 const functions = require('./functions');
 
+//  .toBe
 test('Adds 2 + 2 to equal 4', () => {
     expect(functions.add(2, 2)).toBe(4);
 });
 
+//  .not.toBe
 test('Adds 2 + 2 to NOT equal 5', () => {
     expect(functions.add(2, 2)).not.toBe(5);
 });
@@ -19,12 +21,50 @@ test('Adds 2 + 2 to NOT equal 5', () => {
  *  .toBeTruthy     matches  truthy
  *  .toBeFalsy      matches  falsy
  * 
+ * 
+ *  .toBe will match strict primitives, if you want to match something that isn't a strict primitive such as an array or an object, then use .toEqual.
+ * 
  */
 
+//  .toBeNull 
 test('Should be null', () => {
     expect(functions.isNull()).toBeNull();
 });
 
-test('Should be falsy', () => {
-    expect(functions.checkValue(null)).toBeFalsy();
+//  .toBeFalsy 
+test('User should be falsy', () => {
+    expect(functions.checkFalsy(null)).toBeFalsy();
+});
+
+//  .toEqual
+test('User should be Liam Dawson object', () => {
+    expect(functions.createUser()).toEqual({
+        firstName: "Liam",
+        lastName: "Dawson"
+    });
+});
+
+//  .toBeLessThan
+test('Should be less than 1600', () => {
+    const load1 = 600;
+    const load2 = 700;
+    expect(load1 + load2).toBeLessThan(1600);
+});
+
+//  REGEX
+//  .toMatch
+test('There is no "I" in "Team"', () => {
+    expect('team').not.toMatch(/I|i/);
+});
+
+//  .toMatch
+test('But there is an "I" in "Pie"', () => {
+    expect('pie').toMatch(/I|i/);
+});
+
+//  ARRAYS
+//  .toContain
+test('Admin should be in usernames', () => {
+    usernames = ['Liam', 'Louise', 'Admin', 'Guest'];
+    expect(usernames).toContain('Admin');
 });
